028c9d6aa949fee269ec42913ed825c4b4c3a12f
RandyKoke <thecoachorton@gmail.com>
2026-04-02 23:13:02 +0200

    modification de user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -22,7 +22,7 @@ class AuthController extends Controller
         $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
-            'password' => Hash::make($request->password)
+            'password' => $request->password
         ]);
 
         return response()->json($user);
